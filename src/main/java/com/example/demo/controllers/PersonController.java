9f99386c59fd1223c8642e2fdf6b0012ed6cd14b
musashifraco <ezequielestudos11@gmail.com>
2023-07-07 15:38:51 -0300

    Versionando API Rest, implementando migrations com FlyWay, e adicionando plugin no pom.xml.

--- src/main/java/com/example/demo/controllers/PersonController.java
+++ src/main/java/com/example/demo/controllers/PersonController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import com.example.demo.data.vo.v1.PersonVO;
+import com.example.demo.data.vo.v2.PersonVOV2;
 import com.example.demo.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -30,6 +31,14 @@ public class PersonController {
     )
     public PersonVO create(@RequestBody PersonVO person) {
         return service.create(person);
+    }
+
+    @PostMapping(value = "/v2",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public PersonVOV2 createV2(@RequestBody PersonVOV2 person) {
+        return service.createV2(person);
     }
 
     @PutMapping(
