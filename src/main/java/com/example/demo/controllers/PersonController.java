559e4d225d543cc6f3916102521b2ebc4d8e041b
musashifraco <ezequielestudos11@gmail.com>
2023-08-01 09:20:23 -0300

    Adicionando Serializacao XML via Header.

--- src/main/java/com/example/demo/controllers/PersonController.java
+++ src/main/java/com/example/demo/controllers/PersonController.java
@@ -31,14 +31,6 @@ public class PersonController {
         return service.create(person);
     }
 
-//    @PostMapping(value = "/v2",
-//            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-//            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
-//    )
-//    public PersonVOV2 createV2(@RequestBody PersonVOV2 person) {
-//        return service.createV2(person);
-//    }
-
     @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public PersonVO update(@RequestBody PersonVO person) {
