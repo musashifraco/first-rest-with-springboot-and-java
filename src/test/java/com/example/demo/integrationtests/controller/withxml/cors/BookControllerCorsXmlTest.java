61b8adef389c0a2e5a6cbc3e70394370ee3bf92c
ezequiel.everton <ezequiel.everton@ceuma.br>
2024-07-11 15:50:01 -0300

    feat: adicionando testes de integracao cors para a entidade Book

---
+++ src/test/java/com/example/demo/integrationtests/controller/withxml/cors/BookControllerCorsXmlTest.java
@@ -0,0 +1,160 @@
+package com.example.demo.integrationtests.controller.withxml.cors;
+
+import com.example.demo.configs.TestConfigs;
+import com.example.demo.integrationtests.testcontainers.AbstractIntegrationTest;
+import com.example.demo.integrationtests.vo.AccountCredentialsVO;
+import com.example.demo.integrationtests.vo.BookVO;
+import com.example.demo.integrationtests.vo.TokenVO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
+import static io.restassured.RestAssured.given;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class BookControllerCorsXmlTest extends AbstractIntegrationTest {
+
+
+    private static RequestSpecification specification;
+    private static XmlMapper objectMapper;
+
+    private static BookVO book;
+
+    @BeforeAll
+    public static void setup() {
+        objectMapper = new XmlMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+
+        book = new BookVO();
+    }
+
+    @Test
+    @Order(0)
+    public void authorization() throws JsonMappingException, JsonProcessingException {
+        AccountCredentialsVO user = new AccountCredentialsVO("leandro", "admin123");
+
+        var accessToken = given()
+                .basePath("/auth/signin")
+                .port(TestConfigs.SERVER_PORT)
+                .contentType(TestConfigs.CONTENT_TYPE_XML)
+                .body(user)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(TokenVO.class)
+                .getAccessToken();
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + accessToken)
+                .setBasePath("/api/book/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+    }
+
+    @Test
+    @Order(1)
+    public void testCreateWithWrongOrigin() throws JsonMappingException, JsonProcessingException {
+        mockBook();
+
+        given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_XML)
+                .header(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.WRONG_ORIGIN_EXAMPLE)
+                .body(book)
+                .when()
+                .post()
+                .then()
+                .statusCode(403)
+                .extract()
+                .body()
+                .asString();
+    }
+
+    @Test
+    @Order(2)
+    public void testUpdateWithWrongOrigin() throws JsonMappingException, JsonProcessingException {
+        //book.setLastName("Piquet Souto Maior");
+
+        given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_XML)
+                .header(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.WRONG_ORIGIN_EXAMPLE)
+                .body(book)
+                .when()
+                .post()
+                .then()
+                .statusCode(403)
+                .extract()
+                .body()
+                .asString();
+    }
+
+    @Test
+    @Order(3)
+    public void testFindByIdWithWrongOrigin() throws JsonMappingException, JsonProcessingException {
+        given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_XML)
+                .header(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.WRONG_ORIGIN_EXAMPLE)
+                .pathParam("id", 2L)
+                .when()
+                .get("{id}")
+                .then()
+                .statusCode(403)
+                .extract()
+                .body()
+                .asString();
+    }
+
+    @Test
+    @Order(4)
+    public void testDeleteWithWrongOrigin() throws JsonMappingException, JsonProcessingException {
+        given().spec(specification)
+                .header(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.WRONG_ORIGIN_EXAMPLE)
+                .pathParam("id", 2L)
+                .when()
+                .delete("{id}")
+                .then()
+                .statusCode(403);
+    }
+
+    @Test
+    @Order(5)
+    public void testFindAllWithWrongOrigin() throws JsonMappingException, JsonProcessingException {
+        mockBook();
+
+        given().spec(specification)
+                .header(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.WRONG_ORIGIN_EXAMPLE)
+                .when()
+                .get()
+                .then()
+                .statusCode(403)
+                .extract()
+                .body()
+                .asString();
+    }
+
+    private void mockBook() {
+        book.setAuthor("Author X");
+        book.setPrice(1D);
+        book.setTitle("Title X");
+        book.setLaunchDate(new Date());
+    }
+
+}
