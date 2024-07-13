d983c1f4ef79ad2d7dd19e5c33a57287220e2d02
ezequiel.everton <ezequiel.everton@ceuma.br>
2024-07-13 11:08:18 -0300

    feat: adicionando ordenacao asc e desc (crescente e decrescente)

--- src/main/java/com/example/demo/controllers/PersonController.java
+++ src/main/java/com/example/demo/controllers/PersonController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -60,8 +61,10 @@ public class PersonController {
                                          content = {@Content(mediaType = "application/json",
                                                              array = @ArraySchema(schema = @Schema(implementation = PersonVO.class)))})})
     public ResponseEntity<Page<PersonVO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
-                                                  @RequestParam(value = "limit", defaultValue = "12") Integer limit) throws Exception{
-        Pageable pageable = PageRequest.of(page, limit);
+                                                  @RequestParam(value = "size", defaultValue = "12") Integer size,
+                                                  @RequestParam(value = "direction", defaultValue = "asc") String direction) throws Exception {
+        var sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
 
         return ResponseEntity.ok(service.findAll(pageable));
     }
